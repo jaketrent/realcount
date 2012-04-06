548924aed8099c10f1e96959f66cb6de4db03654
Jake Trent <trent.jake@gmail.com>
2012-04-06 16:59:03 -0600

    links for voting and disp of poll

--- static/js/poll/VoteView.js
+++ static/js/poll/VoteView.js
@@ -1,18 +1,18 @@
-define(['poll/Poll', 'tmpl!poll/poll'], function (Poll, pollTmpl) {
+define(['poll/Poll', 'tmpl!poll/vote'], function (Poll, voteTmpl) {
   return Backbone.View.extend({
     el: '#main',
     initialize: function () {
       this.model = new Poll({
         title_slug: this.options.title_slug
       });
-      this.model.on('success', this.render, this);
+      this.model.on('change', this.render, this);
       this.model.on('error', function () {
         Backbone.Events.trigger('alert', 'Error fetching poll!', 'error');
       });
       this.model.fetch();
     },
     render: function () {
-      this.$el.html('<h1>Voting!</h1>');
+      this.$el.html(voteTmpl(this.model.toJSON()));
     }
   });
 });
