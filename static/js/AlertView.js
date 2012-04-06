7c56b21f83fa0f0baadda9922c55c2ad3bafe04c
Jake Trent <trent.jake@gmail.com>
2012-04-06 10:11:41 -0600

    Clear alerts on page switch

--- static/js/AlertView.js
+++ static/js/AlertView.js
@@ -3,6 +3,10 @@ define(['tmpl!alert'], function(alertTmpl) {
     el: '#alert',
     initialize: function () {
       Backbone.Events.on('alert', this.alert, this);
+      Backbone.Events.on('closeAlert', this.closeAlert, this);
+    },
+    closeAlert: function () {
+      this.$el.html('');
     },
     alert: function (text, type) {
       this.$el.append(alertTmpl({
--- static/js/ViewSwitcher.js
+++ static/js/ViewSwitcher.js
@@ -2,6 +2,7 @@ define(function () {
   return function () {
 
     this.switchView = function (nextView) {
+      Backbone.Events.trigger('closeAlert');
       if (this.lastView !== undefined) {
         this.lastView.close();
       }
